659c9b49eb7ad56b99b5e9bd3e55956d33822fb5
Tungsinn <gvnalamas@tutanota.com>
2021-08-21 18:25:28 -0300

    Atualiza modelos

--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -3,13 +3,14 @@ const { Model, DataTypes } = require('sequelize');
 class Aluno extends Model {
   static init(connection) {
     super.init({
-      codigo: DataTypes.INTEGER,
+      codigo: DataTypes.STRING,
       name: DataTypes.STRING,
       surname: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING,
-      cod_orientador: DataTypes.INTEGER,
+      cod_orientador: DataTypes.STRING,
       curso: DataTypes.STRING,
+      is_ccp: DataTypes.TINYINT,
       created_at: DataTypes.DATE,
     }, {
       sequelize: connection
--- src/models/Orientador.js
+++ src/models/Orientador.js
@@ -3,11 +3,12 @@ const { Model, DataTypes } = require('sequelize');
 class Orientador extends Model {
   static init(connection) {
     super.init({
-      codigo: DataTypes.INTEGER,
+      codigo: DataTypes.STRING,
       name: DataTypes.STRING,
       surname: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING,
+      is_ccp: DataTypes.TINYINT,
       created_at: DataTypes.DATE,
     }, {
       sequelize: connection
