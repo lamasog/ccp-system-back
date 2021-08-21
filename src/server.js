9f4f13934da7aa3f1873a1682dc27af5981c019e
Tungsinn <gvnalamas@tutanota.com>
2021-08-21 16:09:57 -0300

    Alterações

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 });
