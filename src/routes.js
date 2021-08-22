06556775b50939336f4531ab453de5e6d8461ad4
Tungsinn <gvnalamas@tutanota.com>
2021-08-22 01:16:06 -0300

    Alteração das rotas

--- src/routes.js
+++ src/routes.js
@@ -9,16 +9,16 @@ routes.get('/', (req, res) => {
   return res.status(200).json('Rota inicial');
 });
 
-routes.post('/alunos', AlunoController.create);
-routes.get('/alunos', AlunoController.read);
-routes.put('/alunos', Authentication, AlunoController.update);
-routes.delete('/alunos', Authentication, AlunoController.delete);
+routes.post('/alunos/cadastro', AlunoController.create);
 routes.post('/alunos/login', AlunoController.login);
+routes.get('/alunos/list', AlunoController.read);
+routes.put('/alunos/update', Authentication, AlunoController.update);
+routes.delete('/alunos/delete', Authentication, AlunoController.delete);
 
-routes.post('/orientadores', OrientadorController.create);
-routes.get('/orientadores', OrientadorController.read);
-routes.put('/orientadores', Authentication, OrientadorController.update);
-routes.delete('/orientadores', Authentication, OrientadorController.delete);
+routes.post('/orientadores/cadastro', OrientadorController.create);
 routes.post('/orientadores/login', OrientadorController.login);
+routes.get('/orientadores/list', OrientadorController.read);
+routes.put('/orientadores/update', Authentication, OrientadorController.update);
+routes.delete('/orientadores/delete', Authentication, OrientadorController.delete);
 
 module.exports = routes;
