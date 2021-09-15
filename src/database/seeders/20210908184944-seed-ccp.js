ef1493804d7ed3a0ee8904fa49d1652bcd7c153d
Tungsinn <gvnalamas@tutanota.com>
2021-09-15 18:39:12 -0300

    Verifica se a CCP já foi seedada

--- src/database/seeders/20210908184944-seed-ccp.js
+++ src/database/seeders/20210908184944-seed-ccp.js
@@ -5,6 +5,9 @@ require('dotenv').config();
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const ccp = await queryInterface.sequelize.query("SELECT codigo FROM ccps WHERE codigo = '111111'");
+
+    if(ccp[0].length === 0)
     await queryInterface.bulkInsert('ccps', [{
         codigo: '111111',
         name: 'Ccp',
