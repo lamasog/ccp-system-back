cb869e02e5c80b81dcc22b35d37ac02b47cc6a70
Tungsinn <gvnalamas@tutanota.com>
2021-09-17 10:53:35 -0300

    CORS middleware

--- src/app.js
+++ src/app.js
@@ -2,12 +2,20 @@ require('dotenv').config();
 require('./database');
 
 const express = require('express');
-const routes = require('./routes');
 const cors = require('cors');
+const routes = require('./routes');
+
 const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  app.use(cors());
+  next();
+});
+
+// app.use(cors());
 
 app.use(express.json());
 app.use(routes);
-app.use(cors());
 
 module.exports = app;
